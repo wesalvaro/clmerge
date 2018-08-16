e37effe55af9b480b7402dca9e751ad32aacadc2
Wes Alvaro <git@wesalvaro.com>
2018-08-16 23:20:23 +0900

    Make some testing helper functions

--- test.sh
+++ test.sh
@@ -3,49 +3,40 @@ if ! go build; then
   exit 1
 fi
 
-./clmerge \
-  --local tests/conins/SameLine.A.java \
-  --other tests/conins/SameLine.B.java \
-  --base tests/conins/SameLine.X.java \
-  --output /tmp/out.java <<< "
+verify() {
+  local name="$1"
+  local expected="$2"
+  if diff /tmp/clmerge.out "$expected"; then
+    echo "[OK] \c"
+  else
+    echo "[!!] \c"
+  fi
+  echo "$name"
+}
+
+run_sameline() {
+  local name="$1"
+  local output="$2"
+  ./clmerge \
+    --local tests/conins/SameLine.A.java \
+    --other tests/conins/SameLine.B.java \
+    --base tests/conins/SameLine.X.java \
+    --output /tmp/clmerge.out > /dev/null 2>&1
+  verify "$name" "tests/conins/SameLine.O$output.java"
+}
+
+run_sameline "Appetite 1" "" <<< "
 h1
 m
 u
-" > /dev/null 2>&1
-if diff /tmp/out.java tests/conins/SameLine.O.java; then
-  echo "[OK] \c"
-else
-  echo "[!!] \c"
-fi
-echo "Appetite 1"
-
-./clmerge \
-  --local tests/conins/SameLine.A.java \
-  --other tests/conins/SameLine.B.java \
-  --base tests/conins/SameLine.X.java \
-  --output /tmp/out.java <<< "
+"
+run_sameline "Appetite 4" "" <<< "
 h4
 m
 u
-" > /dev/null 2>&1
-if diff /tmp/out.java tests/conins/SameLine.O.java; then
-  echo "[OK] \c"
-else
-  echo "[!!] \c"
-fi
-echo "Appetite 4"
-
-./clmerge \
-  --local tests/conins/SameLine.A.java \
-  --other tests/conins/SameLine.B.java \
-  --base tests/conins/SameLine.X.java \
-  --output /tmp/out.java <<< "
+"
+run_sameline "Appetite 5" ".h5" <<< "
+h5
 m
 u
-" > /dev/null 2>&1
-if diff /tmp/out.java tests/conins/SameLine.O.h5.java; then
-  echo "[OK] \c"
-else
-  echo "[!!] \c"
-fi
-echo "Appetite 5"
+"
