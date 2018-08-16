6b1cc2762ac896b1f806edc2b324bfdfafae2de0
Wes Alvaro <git@wesalvaro.com>
2018-08-16 18:37:31 +0900

    File for automated golden creation.

---
+++ gild_output.sh
@@ -0,0 +1,19 @@
+if ! go build; then
+  echo "Did not build"
+  exit 1
+fi
+
+./clmerge \
+  --output=tests/o.py \
+  --local=tests/a.py \
+  --other=tests/b.py \
+  --base=tests/x.py
+
+./clmerge \
+  --output=tests/conins/SameLine.O.java \
+   --local=tests/conins/SameLine.A.java \
+   --other=tests/conins/SameLine.B.java \
+    --base=tests/conins/SameLine.X.java <<< "
+m
+u
+"
