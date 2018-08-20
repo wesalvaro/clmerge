468881e98c6b142453c8d9979b9111c0bedd3d02
Wes Alvaro <git@wesalvaro.com>
2018-08-20 12:37:23 +0900

    Update output gild script.

--- gild_output.sh
+++ gild_output.sh
@@ -4,17 +4,23 @@ if ! go build; then
 fi
 
 ./clmerge \
-  --output=tests/o.py \
-  --local=tests/a.py \
-  --other=tests/b.py \
-  --base=tests/x.py
+  --output=tests/1/o.py \
+  --local=tests/1/a.py \
+  --other=tests/1/b.py \
+  --base=tests/1/x.py
+
+./clmerge \
+  --output=tests/2/o.go \
+  --local=tests/2/a.go \
+  --other=tests/2/b.go \
+  --base=tests/2/x.go
 
 ./clmerge \
   --appetite=1 \
-  --output=tests/conins/SameLine.O.java \
-   --local=tests/conins/SameLine.A.java \
-   --other=tests/conins/SameLine.B.java \
-    --base=tests/conins/SameLine.X.java <<< "
+  --output=tests/3/SameLine.O.java \
+   --local=tests/3/SameLine.A.java \
+   --other=tests/3/SameLine.B.java \
+    --base=tests/3/SameLine.X.java <<< "
 m
 u
 "
